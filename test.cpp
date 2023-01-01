e156fc194940617558544eadd9f62c2437394ccf
ksalamatian <kave.salamatian@gmail.com>
2023-01-01 22:30:13 +0100

    New version

--- test.cpp
+++ test.cpp
@@ -6,8 +6,10 @@
 using namespace std;
 
 int main(){
-    vector<vector<double>> M{{1,0,1,0},{1,1,0,0},{1,1,0,0}, {1,0,1,0}};
-    vector<double> a{0.25,0.25,0.25, 0.25},b{0.25,0.25,0.25, 0.25};
-    double wd = compute_EMD(a, b, M);
-    cout<<wd<<endl;
+    vector<vector<double>> M{{1,0,1},{2,1,0},{2,2,1}};
+    vector<double> a{0.5,0,0.5},b{0,0.5,0.5};
+    for(int i=0;i<100;i++){
+        double wd = compute_EMD(a, b, M);
+        cout<<wd<<endl;
+    }
 }
